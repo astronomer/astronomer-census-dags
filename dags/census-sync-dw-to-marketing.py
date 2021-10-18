2078c66261f8321e0c42ea6a3ddfe350ede9ff27
Josh Fell <josh.d.fell@astronomer.io>
2021-10-18 09:45:21 -0400

    Add README and update requirements

--- dags/census-sync-dw-to-marketing.py
+++ dags/census-sync-dw-to-marketing.py
@@ -1,31 +1,3 @@
-"""
-### Sync Customer Data from a Data Warehouse to Marketing Campaign Platform with Census
-
-This reverse-ETL pipeline syncs customer data from a Snowflake data warehouse to a marketing platform using
-Census for those customers who last ordered a configurable number of days before the current date. The
-extracted customer information will be used to send personalized emails as part of an ongoing reengagement
-campaign.
-
-#### Configuration
-For configuration purposes, the pipeline uses an Airflow Variable which contains the number of days since a
-customer's last order was placed to send a reengagement email: ``days_since_last_order``. The Variable JSON is
-structured in the following manner:
-
-```
-    {
-        "campaigns": {
-            "reengagement": {
-                "days_since_last_order": 90
-            }
-        }
-    }
-```
-
-#### Connections
-The Airflow Connections used for Census and Snowflake are stored under the default connection IDs,
-"census_default" and "snowflake_default", respectively.
-"""
-
 from datetime import datetime, timedelta
 
 from airflow.decorators import dag
@@ -42,7 +14,6 @@ from airflow_provider_census.sensors.census import CensusSensor
     schedule_interval="@daily",
     catchup=False,
     default_args={"retries": 1, "retry_delay": timedelta(minutes=3)},
-    doc_md=__doc__,
     default_view="graph",
 )
 def census_sync_for_customer_reengagement():
